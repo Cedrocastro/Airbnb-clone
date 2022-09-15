8d53024b4c04ad5f9883e06069aec1f1c185622a
Cedrocastro <110912241+Cedrocastro@users.noreply.github.com>
2022-09-15 09:48:00 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Hero from "./Hero"
+import Card from "./Card"
+import data from "./data"
+import "./style.css"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
